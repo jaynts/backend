f7b71d6041bc555cc7bfd55063e0959eb64545ec
Jayant <jayantsingh2233@gmail.com>
2024-04-18 01:17:46 +0530

    added a route and controller

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}))
 
 app.use(express.static("public"))
 
+//routes
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/user", userRouter);
+
+
 app.use(cookieParser())
 
 export {app}
